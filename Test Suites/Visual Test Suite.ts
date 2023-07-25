27314204e759527c19be915b698151c06180f004
huyen.le <huyen.le@amela.vn>
2023-07-25 10:18:51 +0700

    test

--- Test Suites/Visual Test Suite.ts
+++ Test Suites/Visual Test Suite.ts
@@ -5,10 +5,17 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>9b8cf50c-bad2-426e-8845-5a23c7db8074</testSuiteGuid>
+   <testCaseLink>
+      <guid>89fcc509-c40c-4d1d-a2bf-240a7bf7f706</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Main Test Cases/TC3_Visual Testing Example</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
