93621edb01be496d6a8e63a3190422c8138e1e40
huyen.le <huyen.le@amela.vn>
2023-07-25 10:01:25 +0700

    update

---
+++ Test Suites/Visual Test Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Visual Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>9b8cf50c-bad2-426e-8845-5a23c7db8074</testSuiteGuid>
+</TestSuiteEntity>
